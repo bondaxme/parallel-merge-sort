c56b63c2ecf5a4b59277b7ab0162cead2e9dbed0
maksb <bondax28@gmail.com>
2024-05-24 21:13:52 +0300

    change object

---
+++ src/Building.java
@@ -0,0 +1,17 @@
+public class Building {
+    private String name;
+    private double price;
+
+    public Building(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
